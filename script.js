a46c5c0ad0b72e817cc3a8d3554c571a34b062f6
Antenor <antenorzapata@gmail.com>
2021-03-11 19:17:57 -0300

    final commit

--- script.js
+++ script.js
@@ -1,7 +1,64 @@
+// Elements
+
 const inputText = document.getElementById('carta-texto');
 const btnText = document.getElementById('criar-carta');
 const paragraphText = document.getElementById('carta-gerada');
 const countParagraph = document.getElementById('carta-contador');
+
+// classes group
+
+const styleClass = ['newspaper', 'magazine1', 'magazine2'];
+const sizeClass = ['medium', 'big', 'reallybig'];
+const rotationClass = ['rotateleft', 'rotateright'];
+const skewClass = ['skewleft', 'skewright'];
+
+// generateRandomNumbers
+
+const generateRandom = (len) => Math.floor(Math.random() * len);
+
+// checkStyleCase
+
+const checkStyleCase = (classesGroup, arrClasses, span) => {
+  let indexStyle = generateRandom(styleClass.length);
+  if (arrClasses[0].length < 3) {
+    indexStyle = Math.floor(Math.random() * 2);
+  }
+  span.classList.add(arrClasses[classesGroup][indexStyle]);
+  arrClasses.splice(0, 1);
+};
+
+// checkSizeCase
+
+const checkSizeCase = (classesGroup, arrClasses, span) => {
+  let indexSize = generateRandom(sizeClass.length);
+  if (arrClasses[1].length < 3) {
+    indexSize = Math.floor(Math.random() * 2);
+  }
+  span.classList.add(arrClasses[classesGroup][indexSize]);
+  arrClasses.splice(1, 1);
+};
+
+// checkRotationCase
+
+const checkRotationCase = (classesGroup, arrClasses, span) => {
+  let indexRotation = generateRandom(rotationClass.length);
+  if (arrClasses[2].length < 3) {
+    indexRotation = Math.floor(Math.random() * 2);
+  }
+  span.classList.add(arrClasses[classesGroup][indexRotation]);
+  arrClasses.splice(2, 1);
+};
+
+// checkSkew
+
+const checkSkewCase = (classesGroup, arrClasses, span) => {
+  let indexSkew = generateRandom(skewClass.length);
+  if (arrClasses[3].length < 3) {
+    indexSkew = Math.floor(Math.random() * 2);
+  }
+  span.classList.add(arrClasses[classesGroup][indexSkew]);
+  arrClasses.splice(3);
+};
 
 // handleSwitch
 
