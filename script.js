2528440aca7a7fa4cce23eb047b19f33515ccf61
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-03-12 17:56:46 -0300

    Realizando requisitos 3 e 4

---
+++ script.js
@@ -0,0 +1,17 @@
+const letterInput = document.querySelector('#carta-texto');
+const createBtn = document.querySelector('#criar-carta');
+const letterContainer = document.querySelector('#carta-gerada');
+
+function createLetter() {
+  const words = letterInput.value.trim().split(' ');
+  // Esse é um teste!
+  for (let i = 0; i < words.length; i += 1) {
+    const word = document.createElement('span');
+    word.innerHTML = words[i];
+    letterContainer.appendChild(word);
+  }
+}
+
+window.onload = () => {
+  createBtn.addEventListener('click', createLetter);
+};
