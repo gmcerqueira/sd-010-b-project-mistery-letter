ce7215363fe304bf58347c820d426006b0b96b94
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-03-12 19:08:29 -0300

    Realizando requisito 16

--- script.js
+++ script.js
@@ -2,9 +2,43 @@ const letterInput = document.querySelector('#carta-texto');
 const createBtn = document.querySelector('#criar-carta');
 const letterContainer = document.querySelector('#carta-gerada');
 
+const styles = [
+  'newspaper',
+  'magazine1',
+  'magazine2',
+  'medium',
+  'big',
+  'reallybig',
+  'rotateleft',
+  'rotateright',
+  'skewleft',
+  'skewright',
+];
+// const styles = ['newspaper', 'magazine1', 'magazine2'];
+// const sizes = ['medium', 'big', 'reallybig'];
+// const rotations = ['rotateleft', 'rotateright'];
+// const inclinations = ['skewleft', 'skewright'];
+function randomNumber(max) {
+  return Math.floor(Math.random()*max);
+}
+// function randomClasses() {
+//   const classes = [];
+//   const style = styles[randomNumber(styles.length + 2)];
+//   const size = sizes[randomNumber(sizes.length + 2)];
+//   const rotation = rotations[randomNumber(rotations.length + 2)];
+//   const inclination = inclinations[randomNumber(rotations.length + 2)];
+
+//   if (style) classes.push(style);
+//   if (size) classes.push(size);
+//   if (rotation) classes.push(rotation);
+//   if (inclination) classes.push(inclination);
+
+//   return classes.join(' ');
+// }
+
 function createLetter() {
+  letterContainer.innerHTML = '';
   const words = letterInput.value.trim().split(' ');
-  console.log(words);
   if (!letterInput.value.trim()) {
     letterContainer.innerHTML = 'Por favor, digite o conteúdo da carta.';
     return;
@@ -12,6 +46,8 @@ function createLetter() {
   // Esse é um teste!
   for (let i = 0; i < words.length; i += 1) {
     const word = document.createElement('span');
+    const size = styles.length;
+    word.classList.add(styles[randomNumber(size)], styles[randomNumber(size)], styles[randomNumber(size)]);
     word.innerHTML = words[i];
     letterContainer.appendChild(word);
   }
