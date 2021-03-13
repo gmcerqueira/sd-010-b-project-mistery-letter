efd446a9255270190a00a9ed71a23da6372218f0
Thiago Marchini <thiago@msn.com>
2021-03-13 16:35:28 -0300

    refazendo a escolha de classes e validacao do input

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
+const styles = ['newspaper', 'magazine1', 'magazine2'];
+const size = ['medium', 'big', 'reallybig'];
+const rotation = ['rotateleft', 'rotateright'];
+const inclination = ['skewleft', 'skewright'];
+
+function numberOfClasses() {
+  let numberOfStyleClasses = Math.ceil(Math.random() * 4);
+  while (numberOfStyleClasses === 1) {
+    numberOfStyleClasses = Math.ceil(Math.random() * 4);
+  }
+  return numberOfStyleClasses;
+}
+console.log(numberOfClasses());
+
 function splitText() {
   const textInput = document.getElementById('carta-texto').value;
   const textArray = textInput.split(' ');
   return textArray;
 }
 
-function checkSpaces(input) {
-  const inputArray = input;
-  for (let index = 0; index < inputArray.length; index += 0) {
-    if (inputArray[index] === '') {
-      inputArray.shift();
-    }
-  }
-  return inputArray;
-}
-
-function validateInput() {
-  const textInput = document.getElementById('carta-texto').value;
-  const paragraph = document.getElementById('carta-gerada');
-  let inputArray = splitText();
-  inputArray = checkSpaces(inputArray);
-  if (textInput.length === 0 || inputArray.length === 0) {
-    paragraph.innerText = 'Por favor, digite o conteúdo da carta.';
-  } else {
-    paragraph.innerText = '';
-  }
+function chooseClass() {
+  const typeOfClass = numberOfClasses();
+  
 }
 
 function generateLetter() {
-  validateInput();
-  const words = splitText();
   const parent = document.getElementById('carta-gerada');
+  const words = splitText();
   for (let index = 0; index < words.length; index += 1) {
-    const newSpan = document.createElement('span');
-    newSpan.innerText = words[index];
-    parent.appendChild(newSpan);
+    const span = document.createElement('span');
+    span.innerText = words[index];
+    parent.appendChild(span);
   }
-  // console.log(words);
+
 }
 
 document.getElementById('criar-carta').addEventListener('click', generateLetter);
