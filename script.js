9782e5bfea2c54dfe160521a5aa1895b93d2f240
Thiago Marchini <thiago@msn.com>
2021-03-13 10:57:15 -0300

    inserida a checagem do input por vazio o espaços, e o retorno no paragrafo

--- script.js
+++ script.js
@@ -4,7 +4,30 @@ function splitText() {
   return textArray;
 }
 
+function checkSpaces(input) {
+  const inputArray = input;
+  for (let index = 0; index < inputArray.length; index += 0) {
+    if (inputArray[index] === '') {
+      inputArray.shift();
+    }
+  }
+  return inputArray;
+}
+
+function validateInput() {
+  const textInput = document.getElementById('carta-texto').value;
+  const paragraph = document.getElementById('carta-gerada');
+  let inputArray = splitText();
+  inputArray = checkSpaces(inputArray);
+  if (textInput.length === 0 || inputArray.length === 0) {
+    paragraph.innerText = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    paragraph.innerText = '';
+  }
+}
+
 function generateLetter() {
+  validateInput();
   const words = splitText();
   const parent = document.getElementById('carta-gerada');
   for (let index = 0; index < words.length; index += 1) {
