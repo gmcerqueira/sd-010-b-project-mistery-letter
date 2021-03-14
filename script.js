e502a0ea5b1ecf9c8c4d0a220e72bcb46136a10f
denis-rossati <denis.rossati@hotmail.com>
2021-03-14 00:01:46 -0300

    etapa 3 - try 1

---
+++ script.js
@@ -0,0 +1,43 @@
+const input = document.getElementById('carta-texto');
+const button = document.getElementById('criar-carta');
+const paragraph = document.getElementById('carta-gerada');
+let arrayWords = [];
+
+function pushWords() {
+  let words = '';
+  for (let x = 0; x <= input.value.length; x += 1) {
+    if (input.value[x] !== ' ' && x !== input.value.length) {
+      words += input.value[x];
+    } else {
+      arrayWords.push(words);
+      words = '';
+    }
+  }
+}
+
+function cleanArray() {
+  if (arrayWords.length > 0) {
+    arrayWords = [];
+    pushWords();
+  } else {
+    pushWords();
+  }
+}
+
+function insertWords() {
+  for (let counter = 0; counter < arrayWords.length; counter += 1) {
+    paragraph.appendChild(document.createElement('span'));
+    paragraph.lastChild.innerText = arrayWords[counter];
+  }
+}
+
+function pressButton() {
+  button.addEventListener('click', () => {
+    cleanArray();
+    insertWords();
+  });
+}
+
+window.onload = function startScript() {
+  pressButton();
+};
