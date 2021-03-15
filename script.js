4ce5c18043694c7af26c0a3e971880c9ef406cd4
Alan Tanaka <alanctnk@gmail.com>
2021-03-15 05:05:17 -0300

    Lint problems solved

--- script.js
+++ script.js
@@ -1,39 +1,41 @@
-let btn = document.getElementById('criar-carta')
-let input = document.getElementById('carta-texto')
-let output = document.getElementById('carta-gerada')
-let styleGroup = ['newspaper', 'magazine1', 'magazine2']
-let sizeGroup = ['medium', 'big', 'reallybig']
-let rotationGroup = ['rotateleft', 'rotateright']
-let skewGroup = ['skewleft', 'skewright']
+const btn = document.getElementById('criar-carta');
+const input = document.getElementById('carta-texto');
+const output = document.getElementById('carta-gerada');
+const styleGroup = ['newspaper', 'magazine1', 'magazine2'];
+const sizeGroup = ['medium', 'big', 'reallybig'];
+const rotationGroup = ['rotateleft', 'rotateright'];
+const skewGroup = ['skewleft', 'skewright'];
 
 function createEl(tagName) {
-  let element = document.createElement(tagName)
-  return element
+  const element = document.createElement(tagName);
+  return element;
 }
 
 function getRandom(array) {
-  let len = array.length
-  let index = Math.floor(Math.random() * len)
-  return array[index]
+  const len = array.length;
+  const index = Math.floor(Math.random() * len);
+  return array[index];
 }
 
 // Utilizei código de JONATHAN ROCHA como referência para esta função
-// https://github.com/tryber/sd-010-b-project-mistery-letter/pull/5/commits/5fa6ea9e42310b385982a4dc7fa700320046e8cd 
+// https://github.com/tryber/sd-010-b-project-mistery-letter/pull/5/commits/5fa6ea9e42310b385982a4dc7fa700320046e8cd
 function generateTxt() {
-  output.innerHTML = ""
-  let txt = input.value.replace(/  +/g, ' ').trim()
-  if(txt && txt!==" "){
-    let arr = txt.split(' ')
-    arr.forEach( word => {
-      let span = createEl('span')
-      span.innerText = `${word}`
-      let classes = [getRandom(styleGroup), getRandom(sizeGroup), getRandom(rotationGroup), getRandom(skewGroup)].join(' ')
-      span.className = classes
-      output.appendChild(span)
-    })
+  output.innerHTML = '';
+  const txt = input.value.replace(/  +/g, ' ').trim();
+  if (txt && txt !== ' ') {
+    const arr = txt.split(' ');
+    arr.forEach((word) => {
+      const span = createEl('span');
+      span.innerText = `${word}`;
+      const classes = [
+        getRandom(styleGroup), getRandom(sizeGroup), getRandom(rotationGroup), getRandom(skewGroup),
+      ].join(' ');
+      span.className = classes;
+      output.appendChild(span);
+    });
   } else {
-    output.innerText = "Por favor, digite o conteúdo da carta."
+    output.innerText = 'Por favor, digite o conteúdo da carta.';
   }
 }
 
-btn.addEventListener('click', generateTxt)
+btn.addEventListener('click', generateTxt);
