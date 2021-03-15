d9a35fa7b05ec6c21a362d728b772c69f4e4bc14
Hugo Simoes <hugolfsimoes@gmail.com>
2021-03-14 23:32:37 -0300

    feito a tarefa 5

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
-const inputText = document.querySelector('#carta-texto');
 const paragrafoResultado = document.querySelector('#carta-gerada');
-const btnGerarCarta = document.querySelector('#criar-carta');
-btnGerarCarta.addEventListener('click', () => {
-  removeSpan();
-  const palavrasSeparadas = inputText.value.split(' ');
-  for (let index = 0; index < palavrasSeparadas.length; index += 1) {
-    const spanResultado = document.createElement('span');
-    spanResultado.innerHTML = palavrasSeparadas[index];
-    paragrafoResultado.appendChild(spanResultado);
-  }
-});
 
 function removeSpan() {
   const span = document.querySelectorAll('span');
@@ -17,3 +6,19 @@ function removeSpan() {
     paragrafoResultado.firstChild.remove();
   }
 }
+const inputText = document.querySelector('#carta-texto');
+
+const btnGerarCarta = document.querySelector('#criar-carta');
+btnGerarCarta.addEventListener('click', () => {
+  removeSpan();
+  const palavrasSeparadas = inputText.value.split(' ');
+  for (let index = 0; index < palavrasSeparadas.length; index += 1) {
+    if (palavrasSeparadas[0] !== ' ' && inputText.value !== '') {
+      const spanResultado = document.createElement('span');
+      spanResultado.innerHTML = palavrasSeparadas[index];
+      paragrafoResultado.appendChild(spanResultado);
+    } else {
+      paragrafoResultado.innerHTML = 'Por favor, digite o conteúdo da carta.';
+    }
+  }
+});
