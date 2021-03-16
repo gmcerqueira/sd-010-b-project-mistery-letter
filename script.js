42fd346ba1e66bc56a47d61d3bab224ca042c652
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-16 14:38:40 -0300

    add exercicios 1, 2, 3, 4

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ const outputLetter = document.getElementById('carta-gerada');
 
 function generateLetter() {
   newLetterButton.addEventListener('click', () => {
-    if (inputLetter.value === null || inputLetter.value === '') {
-      outputLetter.innerHTML = 'Por favor, digite o conteúdo da carta.';
+    if (inputLetter.value !== '' || inputLetter === null) {
+      outputLetter.innerHTML = '';
+      const arrayString = inputLetter.value.split(' ');
+      for (let index = 0; index < arrayString.length; index += 1) {
+        const spanWord = document.createElement('span');
+        spanWord.innerHTML = `${arrayString[index]}`;
+        outputLetter.appendChild(spanWord);
+      }
     } else {
-      outputLetter.innerHTML = inputLetter.value;
+      outputLetter.innerHTML = 'Por favor, digite o conteúdo da carta.';
     }
   });
 }
